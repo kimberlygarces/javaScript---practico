cbc9b5212d81b304411535f9e054f7783c6864b8
Kimberly Garcés <kimberlygarces1994@gmail.com>
2021-07-26 20:28:35 -0500

    Metodo reduce - realiza la misma operación del ciclo for de manera reducida

--- promedio.js
+++ promedio.js
@@ -6,10 +6,17 @@
 // ]
 
 function calcularMediaAritmetica(lista){
-    let sumaLista = 0;
-    for(let i = 0; i < lista.length; i++){
-        sumaLista = sumaLista + lista[i];
-    }
+    // let sumaLista = 0;
+    // for(let i = 0; i < lista.length; i++){
+    //     sumaLista = sumaLista + lista[i];
+    // }
+
+    const sumaList = lista.reduce(
+        function(valorAcumulado = 0, nuevoElemento){
+            return valorAcumulado + nuevoElemento;
+        }
+    );
+
 
     const promedioLista = sumaLista / lista.length
     return promedioLista
